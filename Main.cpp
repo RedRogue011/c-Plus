dd9287ce967a80d4e48bc265a569257b5ec82bca
Ashley Bronzan <31910007+RedRogue011@users.noreply.github.com>
2018-01-13 04:09:30 -0500

    added .cpp
    added

---
+++ Main.cpp
@@ -0,0 +1,29 @@
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+
+
+{
+	float Points[10];
+
+	Points[0] = 1;
+	Points[1] = 2;
+	Points[2] = 3;
+	Points[3] = 4;
+	Points[4] = 5;
+	Points[5] = 6;
+	Points[6] = 7;
+	Points[7] = 8;
+	Points[8] = 9;
+	Points[9] = 10;
+
+	cout << Points[3] << endl;
+
+
+}
+
+
